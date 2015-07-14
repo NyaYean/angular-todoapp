a2df2e92ccc66aa68e6835cda83059a5b609dec3
Nya Yeanafehn <Nyeanafehn@gmail.com>
2015-07-14 10:50:17 -0400

    Starting

---
+++ js/app.js
@@ -0,0 +1,16 @@
+angular.module('todo', [])
+
+.controller('ToDoCtrl', ['$scoper', function($scope){
+	$scope.tasklist = [];
+
+	$scope.addTask = function() {
+
+
+	}
+
+	$scope.deleteTask = function() {
+
+		
+	}
+
+}])
