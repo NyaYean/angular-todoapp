670a8b8b3b8e14a2dca13dfa9575c47796ed9415
Nya Yeanafehn <Nyeanafehn@gmail.com>
2015-07-14 12:09:06 -0400

    Added addTask method and ng stuff

--- js/app.js
+++ js/app.js
@@ -1,16 +1,20 @@
 angular.module('todo', [])
 
-.controller('ToDoCtrl', ['$scoper', function($scope){
+.controller('ToDoCtrl', ['$scope', function($scope){
 	$scope.tasklist = [];
 
 	$scope.addTask = function() {
-
-
+		if(event.keyCode == 13 && $scope.taskName) {
+			$scope.tasklist.push({"name": $scope.taskName, "completed": false, 
+			"priority": $scope.priority});
+			$scope.taskName = "";
+			$scope.priority = 'normal';
+		}
 	}
 
 	$scope.deleteTask = function() {
 
-		
+
 	}
 
-}])
+}]);
